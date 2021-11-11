2733a6ad9a9bd645ca7246c1e1703c6635d5b51c
Victor Schneider (Europe) [C] <victor.schneider@global.ntt>
2021-11-11 13:15:50 +0100

    [+] Successful sign up

--- CypressProject/cypress/integration/creating_an_account.js
+++ CypressProject/cypress/integration/creating_an_account.js
@@ -44,14 +44,14 @@ it('Empty fields feedback errors', () => {
 
 it('Signing up for an account with first name, last name and username filled' +
     'but error with passwords', () => {
-    const firstname = 'victor';
-    const lastname = 'schneider';
-    const username = 'schneider';
+    const firstName = 'victor';
+    const lastName = 'schneider';
+    const username = 'ussopn';
     let password = '123';
     let confirmPassword = '123';
 
-    cy.get('#firstName').type(firstname)
-    cy.get('#lastName').type(lastname)
+    cy.get('#firstName').type(firstName)
+    cy.get('#lastName').type(lastName)
     cy.get('#username').type(username)
 
     // error should be visible with a password under 4 letters
@@ -64,5 +64,49 @@ it('Signing up for an account with first name, last name and username filled' +
     cy.get('#confirmPassword').type(confirmPassword)
     cy.get('#confirmPassword-helper-text').should('be.visible')
 
+    // first name, last name and username empty but right confirm password
+    confirmPassword = password
+    cy.get('#firstName').clear()
+    cy.get('#lastName').clear()
+    cy.get('#username').clear()
+    cy.get('#password').clear()
+    cy.get('#confirmPassword').clear()
+    cy.get('#password').type(password)
+    cy.get('#confirmPassword').type(confirmPassword)
+    cy.get('#firstName-helper-text').should('be.visible')
+
+    // last name and username empty, first name filled but correct confirm password
+    cy.get('#firstName').type(firstName)
+    cy.get('#lastName-helper-text').should('be.visible')
+
+    // first name and username empty, last name filled but correct confirm password
+    cy.get('#firstName').clear()
+    cy.get('#lastName').type(lastName)
+    cy.get('#firstName-helper-text').should('be.visible')
+
+    // first name and last name empty, username filled but correct confirm password
+    cy.get('#lastName').clear()
+    cy.get('#username').type(username)
+    cy.get('#firstName-helper-text').should('be.visible')
+
+    // username empty, first name and last name filled but correct confirm password
+    cy.get('#username').clear()
+    cy.get('#firstName').type(firstName)
+    cy.get('#lastName').type(lastName)
+    cy.get('#username-helper-text').should('be.visible')
+
+    // first name empty, last name and username filled but correct confirm password
+    cy.get('#firstName').clear()
+    cy.get('#username').type(username)
+    cy.get('#firstName-helper-text').should('be.visible')
+
+    // last name empty, first name and username filled but correct confirm password
+    cy.get('#lastName').clear()
+    cy.get('#firstName').type(firstName)
+    cy.get('#lastName-helper-text').should('be.visible')
+
+    // Successful sign up
+    cy.get('#lastName').type(lastName)
+    cy.get('[data-test=signup-submit]').click()
 
 });
---
+++ CypressProject/cypress/integration/have_an_account.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"  />
+
+describe('Manage Cookies', () => {
+    it('Cookies', () => {
+        cy.visit('https://www.google.com/')
+        cy.get('#L2AGLb > .jyfHyd').click()
+        cy.getCookies().should('have.length', 2)
+        cy.setCookie('session_id', '189jd09sufh33aaiidhf99d09')
+        cy.getCookie('session_id').should(
+            'have.property',
+            'value',
+            '189jd09sufh33aaiidhf99d09'
+        )
+        cy.clearCookie('session_id')
+        cy.getCookie('session_id').should('be.null')
+    })
+})
